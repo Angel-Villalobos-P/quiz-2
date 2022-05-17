b9f623b79f686a0fddd8bd2f7cc77f19e592bdfd
Angel <angel.villalobos2596@gmail.com>
2022-05-16 21:39:04 -0600

    Routes added to the server

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import routes from "./routes/index.js";
+
+dotenv.config();
+const server = express(); //server
+
+// get body
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+// Data base connection
+const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.lwh3p.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongoose
+  .connect(uri, options)
+  .then(() => console.log("Data base connected"))
+  .catch((e) => console.log("error db:", e));
+
+// routes
+server.use("/", routes);
+
+// route middlewares
+server.get("/", (req, res) => {
+  res.json({
+    estado: true,
+    mensaje: "It works!",
+  });
+});
+
+// Starts server
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server running on: ${PORT}`);
+});
